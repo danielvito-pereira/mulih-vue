6aed51b13bac6b5dc7751ab043198593a896da11
danielvito-pereira <danielvitor081@gmail.com>
2023-03-10 11:40:57 -0300

    Realocação da pasta img

--- src/components/BestRecomendation.vue
+++ src/components/BestRecomendation.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="recomendation">
     <div class="text">
+      <div class="frame">DISCOVER</div>
       <h1 class="h1">Best recommendation</h1>
       <div class="paragaph">
-        <p class="p">Discover our exclusive selection of the finest one-of-a-kind luxury properties architectural masterpieces.</p>
+        <p class="p">Discover our exclusive selection of the finest one-of-a-kind <br>luxury properties architectural masterpieces.</p>
         <p class="learn">Learn More</p>
       </div>
     </div>
       <div class="hotels">
         <div class="hotel">
           <div class="img">
-            <img src="#" alt="hotel" srcset="">
+            <img src="" alt="hotel" srcset="">
           </div>
           <div class="cta">
             <h2 class="h2">Perum griya asri</h2>
@@ -24,7 +25,7 @@
         </div>  
         <div class="hotel">
           <div class="img">
-            <img src="#" alt="hotel" srcset="">
+            <img src="" alt="hotel" srcset="">
           </div>
           <div class="cta">
             <h2 class="h2">Perum griya asri</h2>
@@ -61,5 +62,69 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .recomendation {
+    width: 100%;
+    gap: 3.125rem;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+    padding: 1em;
+    .h1{
+      font-family: 'Rubik';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 48px;
+      line-height: 56px;
+      letter-spacing: 0.5px;
+      color: #054457;
+    }
+    .paragaph{
+      background-color: wheat;
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: top;
+      justify-content: space-between;
+    }
+    .text{
+      width: 90%;
+      max-width: 120rem;
+      background-color:rgb(0, 255, 55);
+      display: flex;
+      flex-flow: column nowrap;
+      gap: 1.125rem;
+    }
+    .hotels{
+      width: 90%;
+      max-width: 120rem;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-rows: 1fr;
+      gap: 0px 0%;
+      grid-auto-flow: row;
+      grid-template-areas:
+        ". . .";
+      gap: 2rem;
+      
+      .hotel{
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid #DEDCDA;
+        padding: 1em;
+        .cta{
+          background-color:rosybrown;
+          width: 100%;
+          .values{
+            display: flex;
+            flex-flow: row nowrap;
+            align-items: center;
+            justify-content: flex-start;
+            gap: 4.125rem;
+          }
+        }
+      }
+    }
+  }
 </style>
